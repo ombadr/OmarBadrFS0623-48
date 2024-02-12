cacc82bc4761244a8198745a1d2d7a70772837e9
omar <omar@DESKTOP-VR2G27Q>
2024-02-12 20:19:48 +0100

    Add project files.

---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Visualizza Candidato</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:Button ID="btnMostraCandidato" runat="server" OnClick="btnMostraCandidato_Click" Text="Mostra Candidato" />
+
+            <asp:Label ID="lblCandidato" runat="server" Text="Label"></asp:Label>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnMostraCandidato_Click(object sender, EventArgs e)
+        {
+            string nome = WebConfigurationManager.AppSettings["CandidatoNome"];
+            string cognome = WebConfigurationManager.AppSettings["CandidatoCognome"];
+
+            lblCandidato.Text = $"Nome: {nome}, Cognome: {cognome}";
+        }
+    }
+}
